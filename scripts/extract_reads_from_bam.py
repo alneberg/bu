e6dc6c9bd28e13df1d5008de3a95bcc33d263ca8
Johannes Alneberg <alneberg@kth.se>
2014-07-24 13:31:47 +0200

    Can use only the bam file to extract all reads

--- scripts/extract_reads_from_bam.py
+++ scripts/extract_reads_from_bam.py
@@ -12,18 +12,22 @@ import sys
 from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
-from pyfaidx import Fasta
 from pysam import Samfile
 
 def main(args):
-    seqs = Fasta(args.reads_file)
     option = "r" if args.samformat else "rb"
     samfile = Samfile(args.bamfile, "rb")
 
     #Iterates over each read instead of each contig
     reads_to_print = []
     for aln in samfile.fetch(until_eof = True):
-        reads_to_print.append(aln.qname)      
+        if args.read_pair == 1 and aln.is_read1:
+            reads_to_print.append(aln)
+        elif args.read_pair == 2 and aln.is_read2:
+            reads_to_print.append(aln)
+        elif args.read_pair == 0:
+            reads_to_print.append(aln)
+
         if len(reads_to_print) >= 10000:
             # Flush the reads collected
             print_reads(reads_to_print)
@@ -33,12 +37,14 @@ def main(args):
 
 def print_reads(reads_to_print):
     # Fetch using the pyfaidx fasta object
-    seq_iter = (SeqRecord(Seq(str(seqs[read])), id=seqs[read].name, description="") for read in reads_to_print)
+    seq_iter = (SeqRecord(Seq(aln.seq), id=aln.qname, description="") for aln in reads_to_print)
     SeqIO.write(seq_iter, sys.stdout, "fasta")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("reads_file", help="Input Fasta file.")
+    parser.add_argument("-r", "--read_pair", type=int, choices=[0, 1, 2],
+            default=0,
+            help="Choose read pair to extract, 1 or 2, if 0 extract all.")
     parser.add_argument("bamfile", help="Input Bam file")
     parser.add_argument("-S", "--samformat", action="store_true", help="Input is in sam format, default is bam format.")
 
