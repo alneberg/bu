7cb1ed69557f128c894a6ae73269fde0087e4832
Johannes Alneberg <alneberg@kth.se>
2018-10-03 15:44:12 +0200

    Change the finishedjobinfo collection to use the extended output

--- scripts/collect_finishedjobinfo.py
+++ scripts/collect_finishedjobinfo.py
@@ -1,51 +1,54 @@
 #!/usr/bin/env python
-DESC="""Script to collect finishedjobinfo statistics for
-        several slurm jobs."""
+"""Script to collect finishedjobinfo statistics for
+   nextflow pipelines ran on uppmax."""
 
 import re
 import os
-import subprocess
-import pandas as p
+import shutil
 from argparse import ArgumentParser
 
+DETAILS_PATH = "/sw/share/slurm/{clustername}/extended_uppmax_jobstats/{node}/{jobid}"
+DETAILS_DEST_FILE = "{node}_{jobid}.stats"
+
 def main(args):
-    """For each file in slurm_dirs, parse out the name
-    call finishedjobinfo for each jobid. """
-    slurm_dir = args.slurm_dir
-    r = re.compile("slurm-([0-9]+)\.out")
-    d = {}
-    for slurm_file in os.listdir(slurm_dir):
-        jobids = r.findall(slurm_file)
-        if len(jobids) != 1:
-            continue
-        jobid = jobids[0]
+    # Check clustername
+    hostname = os.environ.get(['HOSTNAME'])
+    if 'uppmax' not in hostname:
+        raise Exception("This script only works on uppmax, sorry!")
 
-        info = get_info(jobid)
-        d[info['jobid']] = info
-        
-    df = p.DataFrame.from_dict(d, orient="index")
-    df.to_csv(args.output_file)
+    clustername_with_nr = hostname.replace('uppmax.uu.se', '')
+    clustername = clustername_with_nr[:-1]
+    # Fetch the slurmid from the nextflow log
+    # TODO
 
-def get_info(jobid):
-    p = subprocess.Popen(["finishedjobinfo", "-j", jobid], stdout=subprocess.PIPE)
-    info = parse_info(p.stdout.read())
-    return info
+    # run finishedjobinfo on all jobids to get the node it was ran on
+    jobinfo = []
+    with open(args.finishedjobinfo, 'r') as fh:
+        for line in fh:
+            items = line.split(' ')
+            for item in items:
+                # Who needs regexes, huh?
+                if item.startswith('jobid'):
+                    jobid = item.replace('jobid=', '')
+                elif item.startswith('nodes='):
+                    node = item.replace('nodes=', '')
+            jobinfo.append((jobid, node))
 
-def parse_info(s):
-    # Remove date before data
-    data = s.split(" ")[2:]
-    info = {}
-    for datum in data:
-        key = datum.split("=")[0]
-        value = datum.split("=")[1]
-        info[key] = value.strip()
-    return info
+    # fetch the extended run info for each individual job
+    for jobid, node in jobinfo.items():
+        src = DETAILS_PATH.format(clustername=clustername,
+                                  node=node,
+                                  jobid=jobid)
+
+        dest = os.path.join(args.outdir, DETAILS_DEST_FILE.format(node=node,
+                                                                  jobid=jobid))
+        # copy the runinfo to a directory (since it is cleared out regularly)
+        print("Copying, {}, {}".format(src, dest))
+        shutil.copy(src, dest)
 
 if __name__=="__main__":
-    parser = ArgumentParser(description=DESC)
-    parser.add_argument('slurm_dir', help=("Root path where all slurm output files are located, "
-                                           "the names of these files will be used to parse out "
-                                           "the jobid."))
-    parser.add_argument('output_file', help="File where the output table will be printed.")
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('finishedjobinfo', help="File with the finishedjobinfo rows that will be processed")
+    parser.add_argument('outdir', help="Out directory where the stats files will be saved")
     args = parser.parse_args()
     main(args)
