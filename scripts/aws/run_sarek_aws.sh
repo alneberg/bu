8d4c811b50feb3de7c8f94c61dac5954135b8709
alneberg <alneberg@kth.se>
2018-12-21 13:49:01 +0100

    More flexible aws batch launcing script for Sarek

--- scripts/aws/run_sarek_aws.sh
+++ scripts/aws/run_sarek_aws.sh
@@ -1,32 +1,39 @@
-WORKDIR="s3://sarek-work-benchmark/work-tiny_2018_12_13"
-OUTDIR="s3://sarek-result-benchmark/results_tiny_20181213"
-GENOME_BASE="s3://sarek-references/small/"
-REPORT_DIR="../Reports_tiny"
+set -e
+WORKDIR="s3://sarek-work-benchmark-2/work_121221"
+OUTDIR="s3://sarek-results-benchmark-2/results_121221"
+GENOME_BASE="s3://ngi-igenomes/igenomes/Homo_sapiens/GATK/GRCh38"
+AWS_QUEUE="Sarek_pricing_benchmark_11"
+AWS_QUEUE_TINY="Sarek_pricing_benchmark_11_tiny"
+REPORT_DIR="../Reports_dream_121221"
+MAIN_SAMPLE_TSV="Sarek-data/testdata/tsv/dream-test-s3.tsv"
+GENOME="GRCh38"
 
-COMMON_PARAMS="-profile awsbatch --awsqueue $AWS_QUEUE --awsqueue_tiny $AWS_QUEUE_TINY -work-dir $WORKDIR --outDir
-$OUTDIR --verbose -resume"
+#Comment away this when running real data
+#WORKDIR="s3://sarek-work-benchmark/work-tiny_20181221"
+#OUTDIR="s3://sarek-result-benchmark/results_tiny_20181221"
+#GENOME_BASE="s3://sarek-references/small/"
+#REPORT_DIR="../Reports_tiny"
+#MAIN_SAMPLE_TSV="Sarek-data/testdata/tsv/tiny-s3.tsv"
+#GENOME="smallGRCh37"
+
+COMMON_PARAMS="-profile awsbatch --awsqueue $AWS_QUEUE --awsqueue_tiny $AWS_QUEUE_TINY -work-dir $WORKDIR --outDir $OUTDIR --verbose -resume"
+
 
 STEP=main
 echo $STEP
-echo nextflow run main.nf $COMMON_PARAMS --localReportdir ${REPORT_DIR}_${STEP} --sample Sarek-data/testdata/tsv/tiny-s3.tsv --genome smallGRCh37 --genome_base $GENOME_BASE 
+nextflow run main.nf $COMMON_PARAMS --localReportDir ${REPORT_DIR}_${STEP} --sample $MAIN_SAMPLE_TSV  --genome $GENOME --genome_base $GENOME_BASE 
 
 STEP=germlineVC
 echo $STEP
-echo nextflow run germlineVC.nf $COMMON_PARAMS --localReportdir ${REPORT_DIR}_${STEP} --sample $OUTDIR/Preprocessing/Recalibrated/recalibrated.tsv --genome smallGRCh37 --genome_base $GENOME_BASE --tools HaplotypeCaller,Manta,Strelka
+nextflow run germlineVC.nf $COMMON_PARAMS --localReportDir ${REPORT_DIR}_${STEP} --sample $OUTDIR/Preprocessing/Recalibrated/recalibrated.tsv --genome $GENOME --genome_base $GENOME_BASE --tools HaplotypeCaller,Manta,Strelka
 
 STEP=annotate
 echo $STEP
-echo nextflow run annotate.nf $COMMON_PARAMS --localReportdir ${REPORT_DIR}_${STEP} --genome smallGRCh37 --genome_base $GENOME_BASE --annotateTools HaplotypeCaller,Strelka --tools snpEFF,VEP
+nextflow run annotate.nf $COMMON_PARAMS --localReportDir ${REPORT_DIR}_${STEP} --genome $GENOME --genome_base $GENOME_BASE --annotateTools HaplotypeCaller,Strelka --tools snpEFF,VEP
 
 STEP=multiqc
 echo $STEP
-echo nextflow run runMultiQC.nf $COMMON_PARAMS --localReportdir ${REPORT_DIR}_${STEP}
+nextflow run runMultiQC.nf $COMMON_PARAMS --localReportDir ${REPORT_DIR}_${STEP}
 
-WORKDIR="s3://sarek-work-benchmark/work-dream-ssd_2018_12_12"
-OUTDIR="s3://sarek-result-benchmark/results_dream_ssd_20181212"
-GENOME_BASE="s3://ngi-igenomes/igenomes/Homo_sapiens/GATK/GRCh38"
-AWS_QUEUE="Sarek_pricing_benchmark_10"
-AWS_QUEUE_TINY="Sarek_pricing_benchmark_10_tiny"
-REPORT_DIR="Reports"
 
 
