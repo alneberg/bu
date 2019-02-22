6dd11208b526d4e65f4684a3ec05299dd0b25f8d
alneberg <alneberg@kth.se>
2019-02-22 08:52:00 +0100

    force creation of new link target when transfering to irma

--- scripts/uppmax_utilities/transfer_repo_to_irma.py
+++ scripts/uppmax_utilities/transfer_repo_to_irma.py
@@ -75,7 +75,7 @@ def main(args):
         link_name = "{}_dev".format(dirname)
     else:
         link_name = "{}_latest".format(dirname)
-    c.run('cd {}; ln -s {} {}'.format(remote_dir, remote_extracted_path, link_name))
+    c.run('cd {}; ln -sf {} {}'.format(remote_dir, remote_extracted_path, link_name))
     logger.info("{} successfully linked as the new {}".format(dirname, link_name))
 
 if __name__ == '__main__':
