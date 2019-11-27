aeeddd9c4f7ee87c5cca2b2dd5d5f396290285b6
Johannes Alneberg <alneberg@kth.se>
2019-11-27 16:34:52 +0100

    Check if log file directory exists

--- templates/log_to_file.py
+++ templates/log_to_file.py
@@ -5,13 +5,21 @@ A file gets the detailed logs while stderr gets warnings, errors and critical.
 """
 
 import logging
+import os
+import sys
 
 def main():
     logger.info("First entry as INFO")
     logger.warning("Second entry as WARNING")
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="logging_test.txt", level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logfile = "logging_test.txt"
+    logfile_directory = os.path.dirname(os.path.abspath(logfile))
+    if not os.path.exists(logfile_directory):
+        logging.error("The directory for the specified log file does not exist. Aborting")
+        sys.exit(-1)
+
+    logging.basicConfig(filename=logfile, level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     logger = logging.getLogger(__name__)
 
     # Handler that will log warnings or worse
